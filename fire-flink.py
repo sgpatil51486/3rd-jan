b0564ac43bb74e3fddb5a436034ee1d078871956
sgpatil51486 <119474716+sgpatil51486@users.noreply.github.com>
2023-01-03 12:59:09 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -19,7 +19,7 @@ def login(token):
     }
     head["Authorization"] = "Bearer " + token
     suiteid = 'SUITE1193'
-    baseUrl = 'http://10.10.10.170:9091'
+    baseUrl = 'http://10.10.10.170'
 
     run = s.get(baseUrl + ':8102/optimize/v1/suite/runSetting/run/' + suiteid, headers=head)
     runout = json.loads(run.content)
